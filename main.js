1016400c25467d44ecaeda43eeb8490290f725c7
hasanmd91 <hasanmd1991@hotmail.com>
2022-10-19 19:56:16 +0300

    hero section ok

--- main.js
+++ main.js
@@ -3,9 +3,32 @@ const burger = document.querySelector(".burger");
 const navLinks = document.querySelectorAll("li");
 const bars = document.querySelector("#bars");
 const cross = document.querySelector("#cross");
+const links = document.querySelectorAll(".link");
+const body = document.body;
+// scroll event
+
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  let currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove("scroll-up");
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains("scroll-down")) {
+    body.classList.remove("scroll-up");
+    body.classList.add("scroll-down");
+  }
+
+  if (currentScroll < lastScroll && !body.classList.contains("scroll-up")) {
+    body.classList.remove("scroll-down");
+    body.classList.add("scroll-up");
+  }
+  lastScroll = currentScroll;
+});
 
 //burger menu function
-console.log(navLinks);
+
 burger.addEventListener("click", () => {
   ul.classList.toggle("nav-active");
   navLinks.forEach((link, index) => {
@@ -16,5 +39,20 @@ burger.addEventListener("click", () => {
         index / 7 + 0.3
       }s `;
     }
+  });
+});
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    ul.classList.remove("nav-active");
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `fadeNavLinks 0.3s ease forwards ${
+          index / 7 + 0.3
+        }s `;
+      }
+    });
   });
 });
